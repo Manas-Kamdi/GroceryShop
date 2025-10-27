ee8887566d0cb1e8031ce1a0f56e98449e0bde76
Manas-Kamdi <kamdimanas@gmail.com>
2025-10-27 22:20:09 +0530

    update are taken place

--- GroceryShop/settings.py
+++ GroceryShop/settings.py
@@ -95,11 +95,11 @@ USE_TZ = True
 # =========================
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']  # optional for local dev
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # =========================
 # Security for HTTPS
